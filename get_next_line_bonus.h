273cb2ad54681a7a3051676e29fa40eb643c3c0e
Marc Faussurier <mafaussu@z1r5p4.42lyon.fr>
2022-02-16 11:29:59 +0100

    fixes

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,12 +6,12 @@
 /*   By: mafaussu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 10:08:29 by mafaussu          #+#    #+#             */
-/*   Updated: 2022/02/15 10:08:59 by mafaussu         ###   ########lyon.fr   */
+/*   Updated: 2022/02/15 12:35:15 by mafaussu         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 char	*get_next_line(int fd);
 
